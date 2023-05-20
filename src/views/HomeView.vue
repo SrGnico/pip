af10e92d27d99180737f082452465d2acb125db2
SrGnico <srgnicor@gmail.com>
2023-05-20 19:47:03 -0300

    Added qr code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="reader" width="600px"></div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+  // To use Html5QrcodeScanner (more info below)
+import {Html5QrcodeScanner} from "html5-qrcode";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
 }
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 </script>
+
+<style>
+</style>
